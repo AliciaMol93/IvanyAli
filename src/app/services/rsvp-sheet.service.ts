7663e040a1c12b1e7f4625c48688efbbb2f2c5e9
Alicia Martín <alicia.93@hotmail.com>
2025-12-12 16:16:11 +0100

    Add Google Sheets URL2

--- src/app/services/rsvp-sheet.service.ts
+++ src/app/services/rsvp-sheet.service.ts
@@ -1,30 +1,27 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Invitado } from '../components/rsvp/rsvp-form/invitadoInterface';
-
 
 @Injectable({ providedIn: 'root' })
 export class RsvpSheetService {
 
-  private apiUrl = 'https://script.google.com/macros/s/AKfycbxYYScQrj2YFUlpa-INitV-b6Lnp5gq_xPh4HRsujvmkV1i-xt3WSVtfa2NgSSzlyA50g/exec';
-
+  private apiUrl = 'https://script.google.com/macros/s/AKfycbxZ8hZaTQwGz_L0anKsDxtOOFfzZ3FhOKC78k-LC-okx6j2k3YcR0KgfCnU8VklJKiGuQ/exec';
 
   constructor(private http: HttpClient) {}
 
   guardarInvitado(invitado: Invitado) {
-    return this.http.post(this.apiUrl, {
-      type: 'invitado',
-      ...invitado
-    });
+    return this.http.post(
+      this.apiUrl,
+      { type: 'invitado', ...invitado },
+      { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) }
+    );
   }
 
   guardarSugerencia(invitado_id: number, email: string, song_name: string, artist: string) {
-    return this.http.post(this.apiUrl, {
-      type: 'sugerencia',
-      invitado_id,
-      email,
-      song_name,
-      artist
-    });
+    return this.http.post(
+      this.apiUrl,
+      { type: 'sugerencia', invitado_id, email, song_name, artist },
+      { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) }
+    );
   }
 }
